84e034e0a02151e15f3e62fd023d3e5e6b9c9867
JieniWu <jwu5@colgate.edu>
2026-01-14 16:33:55 +0800

    Initial commit

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,12 @@
+import styles from './newissue.module.css';
+
+const NewIssuePage = () => {
+    return (
+        <div>
+            <button className={styles.button}>New Issue</button>
+        </div>
+        
+    )
+}
+
+export default NewIssuePage;
---
+++ lib/dal.ts
@@ -0,0 +1,53 @@
+import { db } from '@/db'
+import { getSession } from './auth'
+import { eq } from 'drizzle-orm'
+import { cache } from 'react'
+import { issues, users } from '@/db/schema'
+import { mockDelay } from './utils'
+
+export const getCurrentUser = async () => {
+    await mockDelay(1000) 
+    const session = await getSession()
+    if (!session) {
+        return null
+    }
+    try{
+        const results = await db
+        .select()
+        .from(users)
+        .where(eq(users.id, session.userId))
+        return results[0] || null
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
+
+export const getUserByEmail = async (email: string) => {
+    try{
+        const user = await db
+        .query.users.findFirst({
+            where: eq(users.email, email)
+        })
+        return user
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
+
+export async function getIssues() {
+  try {
+    await mockDelay(1000) // Simulate network delay
+    const result = await db.query.issues.findMany({
+      with: {
+        user: true,
+      },
+      orderBy: (issues, { desc }) => [desc(issues.createdAt)],
+    })
+    return result
+  } catch (error) {
+    console.error('Error fetching issues:', error)
+    throw new Error('Failed to fetch issues')
+  }
+}
